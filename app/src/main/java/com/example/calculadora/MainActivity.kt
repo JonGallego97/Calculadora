239b3fb4f498a13ecb9bb147da420a8e37ba936c
in2dm3-v <ibai.gonzalezug@elorrieta-errekamari.com>
2023-09-14 18:34:37 +0200

    0.2

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -2,59 +2,66 @@ package com.example.calculadora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    var contador: Int = 0
+    private var currentInput = ""
+    private var currentResult = 0.0
+    private lateinit var resultTextView: TextView
+    private var operation = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button: Button = findViewById(R.id.button5)
-        val text: TextView = findViewById(R.id.result)
+        findViewById <Button> (R.id.button0).setOnClickListener { handleNumberClick("0") }
+        findViewById <Button> (R.id.button1).setOnClickListener { handleNumberClick("1") }
+        findViewById <Button> (R.id.button2).setOnClickListener { handleNumberClick("2") }
+        findViewById <Button> (R.id.button3).setOnClickListener { handleNumberClick("3") }
+        findViewById <Button> (R.id.button4).setOnClickListener { handleNumberClick("4") }
+        findViewById <Button> (R.id.button5).setOnClickListener { handleNumberClick("5") }
+        findViewById <Button> (R.id.button6).setOnClickListener { handleNumberClick("6") }
+        findViewById <Button> (R.id.button7).setOnClickListener { handleNumberClick("7") }
+        findViewById <Button> (R.id.button8).setOnClickListener { handleNumberClick("8") }
+        findViewById <Button> (R.id.button9).setOnClickListener { handleNumberClick("9") }
+        findViewById <Button> (R.id.buttonComa).setOnClickListener { handleNumberClick(".") }
+        findViewById <Button> (R.id.buttonDelete).setOnClickListener { handleNumberClick("1") }
+        findViewById <Button> (R.id.buttonDiv).setOnClickListener { onOperationClick("/") }
+        findViewById <Button> (R.id.buttonMinus).setOnClickListener { onOperationClick("-") }
+        findViewById <Button> (R.id.buttonPlus).setOnClickListener { onOperationClick("+") }
+        findViewById <Button> (R.id.buttonEqual).setOnClickListener { onEqualsClick() }
 
-        button.setOnClickListener {
-            contador++
-            text.text = contador.toString()
+    }
+
+    private fun onEqualsClick() {
+        if (operation.isEmpty()) {
+            resultTextView.text = currentInput
+        } else {
+            when(operation){
+                "+" -> currentResult += currentInput.toDouble()
+                "-" -> currentResult -= currentInput.toDouble()
+                "*" -> currentResult *= currentInput.toDouble()
+                "/" -> currentResult /= currentInput.toDouble()
+            }
         }
     }
 
-    override fun onResume() {
-        super.onResume()
-        Log.d("Debug", "onResume")
-        val text: TextView = findViewById(R.id.result)
-        text.text = contador.toString()
+    private fun handleNumberClick(number: String) {
+        currentInput += number
+        updateResult()
     }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        outState?.run {
-            putInt("contador", contador)
-        }
-        super.onSaveInstanceState(outState)
+    private fun updateResult() {
+        resultTextView.text = currentInput
     }
-    override fun onPause() {
-        super.onPause()
-        Log.d("Debug", "onPause")
-    }
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d("Debug", "onDestroy")
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        savedInstanceState?.run {
-            contador = savedInstanceState.getInt("contador")
-            Log.d("Debug", "onRestoreInstanceState: $contador")
-        }
+    private fun onOperationClick(selectedOperation: String) {
+        operation = selectedOperation
+        currentResult = currentInput.toDouble()
+        currentInput = ""
+        updateResult()
 
     }
-    override fun onStart() {
-        super.onStart()
-        Log.d("Debug", "onStart")
-    }
+
 }
